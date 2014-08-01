85408b1a3be48e20977551e024d71ee20aa3dd94
Steve Osmun <sosmun13@gmail.com>
2014-07-31 20:49:02 -0700

    Added Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get "about" => "pages#about"
+  get "clutch" => "pages#clutch"
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
